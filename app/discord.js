8846b963d66b1a8634f6f25e8b24360e7242af8a
zvbt <zvbt@pm.me>
2023-07-10 05:09:18 +0100

    fixed rpc in a wierd way but it work

--- app/discord.js
+++ app/discord.js
@@ -11,12 +11,35 @@ discordClient.on('connected', () => {
 
 module.exports = function discord(mainWindow) {
   let title = mainWindow.getTitle()
-  if (title.match('by')) {
-    const music_title = title.split('by').shift()
-    const artist = title.split('by').pop()
+
+  if (title.match(' by ')) {
+    const music_title = title.split(' by ').shift()
+    const artist = title.split(' by ').pop()
     discordClient.updatePresence({
-      state: `by ${artist}`,
-      details: `${music_title}`,
+      state: `${music_title} by ${artist}`,
+      details: `Listening`,
+      largeImageKey:
+        'https://r2.e-z.host/7ed0180f-b228-49a7-be1e-0183c1938777/81gpstko.png',
+      smallImageKey: 'none',
+      instance: false,
+    })
+  }
+  if (title.match(' in ')) {
+    const music_title_pl = title.split(' in ').shift()
+    const playlist = title.split(' in ').pop()
+    discordClient.updatePresence({
+      state: `in ${playlist}`,
+      details: `Listening ${music_title_pl}`,
+      largeImageKey:
+        'https://r2.e-z.host/7ed0180f-b228-49a7-be1e-0183c1938777/81gpstko.png',
+      smallImageKey: 'none',
+      instance: false,
+    })
+  }
+  if (title.match(' on Soundcloud')) {
+    discordClient.updatePresence({
+      state: `          `,
+      details: `Browsing`,
       largeImageKey:
         'https://r2.e-z.host/7ed0180f-b228-49a7-be1e-0183c1938777/81gpstko.png',
       smallImageKey: 'none',
